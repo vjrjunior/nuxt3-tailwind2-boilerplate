50ff51bf1ad789455488c4232db2024544ad8a5b
Igor Trindade <igortrindademe@gmail.com>
2021-11-21 10:45:24 -0300

    Setup everything

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-screen h-screen bg-green-900 flex flex-col items-center justify-center space-y-2">
+    <h1 class="text-white text-4xl font-bold">Nuxt3 + Tailwind2 Boilerplate</h1>
+    <a class="underline text-green-600 text-3xl font-bold" href="https://github.com/igortrinidad/nuxt3-tailwind2-boilerplate">Github</a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import './assets/main.css'
+</script>
