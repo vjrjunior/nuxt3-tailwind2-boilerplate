81cc3bdbe882e05515697d378df2a15687d57c4d
vjrjunior <vjrjunior@gmail.com>
2021-12-23 10:03:16 -0300

    change nuxt.config base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,20 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-
+  
+  head: {
+    titleTemplate: '%s | {{ name }}',
+    htmlAttrs: { lang: 'en' },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '{{escape description }}' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preload', as: 'style', href: '~/assets/scss/main.scss', onload: "this.onload=null;this.rel='stylesheet'", },
+    ],
+  },
   build: {
     postcss: {
       postcssOptions: {
@@ -12,6 +25,7 @@ export default defineNuxtConfig({
         }
       }
     },
-  }
+  },
+  css: ['assets/scss/main.scss'],
   
 })
